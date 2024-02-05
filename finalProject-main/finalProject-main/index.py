8971f4b9eef4acfef0d10d998702514e4a2acf31
CAD-8 <artemysttr@gmail.com>
2024-02-05 19:23:23 +0400

    first

--- finalProject-main/finalProject-main/index.py
+++ finalProject-main/finalProject-main/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -19,6 +19,15 @@ def promotion():
     a = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
          'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
     return '</br>'.join(a)
+
+
+@app.route('/mars_image')
+def image():
+    return f'''
+    <h1>Жди нас, Марс!</h1>
+    <img src="{url_for('static', filename='img/mars.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась">
+    <p>Вот она какая, красная планета</p>'''
 
 
 if __name__ == '__main__':
