fb2c25baf280774c6126cf3faad0088e29d4d8f2
CAD-8 <artemysttr@gmail.com>
2024-02-05 19:39:12 +0400

    first

--- finalProject-main/finalProject-main/index.py
+++ finalProject-main/finalProject-main/index.py
@@ -36,6 +36,7 @@ def bootstrap():
                 <html lang="en">
                   <head>
                     <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
                     <link rel="stylesheet" 
                     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
